cd75ab31641ce2ff2bd1c1bf96a66b3c653f6bda
Wang Xuerui <idontknw.wang@gmail.com>
2015-09-11 01:42:29 +0800

    sched_policy: be compatible with stock Flyme boot.img
    Change-Id: I3e1cacc4277fc19f8391d8881e188c35cbbfa848

--- libcutils/sched_policy.c
+++ libcutils/sched_policy.c
@@ -110,16 +110,16 @@ static int add_tid_to_cgroup(int tid, SchedPolicy policy)
 
 static void __initialize(void) {
     char* filename;
-    if (!access("/dev/cpuctl/tasks", F_OK)) {
+    if (!access("/dev/cpuctl/apps/tasks", F_OK)) {
         __sys_supports_schedgroups = 1;
 
-        filename = "/dev/cpuctl/tasks";
+        filename = "/dev/cpuctl/apps/tasks";
         fg_cgroup_fd = open(filename, O_WRONLY | O_CLOEXEC);
         if (fg_cgroup_fd < 0) {
             SLOGE("open of %s failed: %s\n", filename, strerror(errno));
         }
 
-        filename = "/dev/cpuctl/bg_non_interactive/tasks";
+        filename = "/dev/cpuctl/apps/bg_non_interactive/tasks";
         bg_cgroup_fd = open(filename, O_WRONLY | O_CLOEXEC);
         if (bg_cgroup_fd < 0) {
             SLOGE("open of %s failed: %s\n", filename, strerror(errno));
@@ -214,9 +214,9 @@ int get_sched_policy(int tid, SchedPolicy *policy)
         char grpBuf[32];
         if (getSchedulerGroup(tid, grpBuf, sizeof(grpBuf)) < 0)
             return -1;
-        if (grpBuf[0] == '\0') {
+        if (!strcmp(grpBuf, "apps")) {
             *policy = SP_FOREGROUND;
-        } else if (!strcmp(grpBuf, "bg_non_interactive")) {
+        } else if (!strcmp(grpBuf, "apps/bg_non_interactive")) {
             *policy = SP_BACKGROUND;
         } else {
             errno = ERANGE;
